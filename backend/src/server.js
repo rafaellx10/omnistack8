b0bc00fcc7623ccd5db320ebcd454da9c2f43837
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-08-04 23:19:19 -0300

    response json

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const server = express();
 
 server.get('/', (request, response) => {
-  return response.send(`Hello ${request.query.name}`);
+  return response.json({ message: `Hello ${request.query.name}` });
   //URL: http://localhost:3333/?name=Rafael
 });
 
