1049533c0404f4819b9d889d8fd3876908943e55
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-08-04 23:31:33 -0300

    spliting routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,8 @@
 const express = require('express');
+const routes = require('./routes');
 
 const server = express();
 
-server.get('/', (request, response) => {
-  return response.json({ message: `Hello ${request.query.name}` });
-  //URL: http://localhost:3333/?name=Rafael
-});
+server.use(routes);
 
 server.listen(3333);
