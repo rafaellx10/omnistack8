a7ef113681c337045842210bc202f20110d84a0f
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-08-21 19:58:31 -0300

    create like and dislike routes

---
+++ backend/src/controllers/DislikeController.js
@@ -0,0 +1,25 @@
+const Dev = require('../model/Dev');
+
+module.exports = {
+  async store(req, res) {
+    // console.log(req.params.devId); // capturar o parametro na URL
+    // console.log(req.headers.user); // capturar o headers user
+    // return res.json({ ok: true });
+    const { user } = req.headers;
+    const { devId } = req.params;
+
+    const loggedDev = await Dev.findById(user);
+    const targedDev = await Dev.findById(devId);
+    console.log(targedDev);
+
+    if (!targedDev) {
+      return res.status(400).json({ error: 'Dev not exists' });
+    }
+
+    loggedDev.dislikes.push(targedDev._id);
+
+    await loggedDev.save();
+
+    return res.json({ loggedDev });
+  },
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 
 const routes = express.Router();
 
@@ -9,5 +11,7 @@ routes.get('/', (request, response) => {
 });
 
 routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
 module.exports = routes;
