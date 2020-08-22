42cc9dc060833f28539b4797682bce2960f29dbb
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-08-22 19:23:20 -0300

    add match feature

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -16,6 +16,10 @@ module.exports = {
       return res.status(400).json({ error: 'Dev not exists' });
     }
 
+    if (targedDev.likes.includes(loggedDev._id)) {
+      console.log('Deu MATCH');
+    }
+
     loggedDev.likes.push(targedDev._id);
 
     await loggedDev.save();
