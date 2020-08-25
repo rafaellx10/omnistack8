0040979ba11005fc8b3609a5556572bd77efb855
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-08-25 19:18:21 -0300

    list all devs on main page

--- frontend/react-project/src/pages/Main.js
+++ frontend/react-project/src/pages/Main.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
@@ -6,97 +8,52 @@ import dislike from '../assets/dislike.svg';
 import './Main.css';
 
 export default function Main({ match }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // outra maneira de fazer uma async function
+    // (async function loadUsers() {
+    // })();
+    async function loadUsers() {
+      const response = await api.get('/devs', {
+        headers: {
+          user: match.params.id,
+        },
+      });
+      setUsers(response.data);
+    }
+
+    loadUsers();
+  }, [match.params.id]);
+
+  async function handleLike(id) {
+    console.log('Like', id);
+  }
+
+  async function handleDislike(id) {
+    console.log('Dislike', id);
+  }
   return (
     <div className="main-container">
       <img src={logo} alt="Tindev" />
       <ul>
-        <li>
-          <img
-            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
-            alt=""
-          />
-          <footer>
-            <strong>Diego Fernandes</strong>
-            <p>
-              CTO at @Rocketseat. Passionate about education and changing
-              people's lives through programming.CTO at @Rocketseat. Passionate
-              about education and changing people's lives through programming.
-              CTO at @Rocketseat. Passionate about education and changing
-              people's lives through programming.
-            </p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={like} alt="like" />
-            </button>
-            <button type="button">
-              <img src={dislike} alt="dislike" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
-            alt=""
-          />
-          <footer>
-            <strong>Diego Fernandes</strong>
-            <p>
-              CTO at @Rocketseat. Passionate about education and changing
-              people's lives through programming.
-            </p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={like} alt="like" />
-            </button>
-            <button type="button">
-              <img src={dislike} alt="dislike" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
-            alt=""
-          />
-          <footer>
-            <strong>Diego Fernandes</strong>
-            <p>
-              CTO at @Rocketseat. Passionate about education and changing
-              people's lives through programming.
-            </p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={like} alt="like" />
-            </button>
-            <button type="button">
-              <img src={dislike} alt="dislike" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
-            alt=""
-          />
-          <footer>
-            <strong>Diego Fernandes</strong>
-            <p>
-              CTO at @Rocketseat. Passionate about education and changing
-              people's lives through programming.
-            </p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={like} alt="like" />
-            </button>
-            <button type="button">
-              <img src={dislike} alt="dislike" />
-            </button>
-          </div>
-        </li>
+        {users.map((user) => (
+          <li key={user._id}>
+            <img src={user.avatar} alt={user.name} />
+            <footer>
+              <strong>{user.name}</strong>
+              <p>{user.bio}</p>
+            </footer>
+            <div className="buttons">
+              <button type="button" onClick={() => handleDislike(user._id)}>
+                <img src={dislike} alt="dislike" />
+              </button>
+              <button type="button" onClick={() => handleLike(user._id)}>
+                <img src={like} alt="like" />
+              </button>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
