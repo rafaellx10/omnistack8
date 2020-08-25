d32b4daaeed0db0cf2f3eeec1cdd783218442530
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-08-25 15:32:36 -0300

    using useState on login

--- frontend/react-project/src/pages/Login.js
+++ frontend/react-project/src/pages/Login.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 
 import logo from '../assets/logo.svg';
 
 function Login() {
+  const [username, setUsername] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(username);
+  }
+
   return (
     <div className="login-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <img src={logo} alt="Tindev" />
-        <input type="text" placeholder="Digite seu usuário no Github" />
+        <input
+          type="text"
+          placeholder="Digite seu usuário no Github"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
         <button type="submit">Enviar</button>
       </form>
     </div>
