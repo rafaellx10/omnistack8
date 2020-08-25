5b7e920946a087845505595e7aad3aac65d92e24
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-08-25 00:12:43 -0300

    create Login page

---
+++ frontend/react-project/src/pages/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Login.css';
+
+import logo from '../assets/logo.svg';
+
+function Login() {
+  return (
+    <div className="login-container">
+      <form>
+        <img src={logo} alt="Tindev" />
+        <input type="text" placeholder="Digite seu usuário no Github" />
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
