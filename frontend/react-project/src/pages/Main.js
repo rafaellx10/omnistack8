fdf841760f91fe714609be19dcf7b40b53e3b0e7
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-08-25 17:27:47 -0300

    add style to the main page

--- frontend/react-project/src/pages/Main.js
+++ frontend/react-project/src/pages/Main.js
@@ -1,5 +1,103 @@
 import React from 'react';
 
-export default function Main() {
-  return <h1>Hello</h1>;
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+import './Main.css';
+
+export default function Main({ match }) {
+  return (
+    <div className="main-container">
+      <img src={logo} alt="Tindev" />
+      <ul>
+        <li>
+          <img
+            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
+            alt=""
+          />
+          <footer>
+            <strong>Diego Fernandes</strong>
+            <p>
+              CTO at @Rocketseat. Passionate about education and changing
+              people's lives through programming.CTO at @Rocketseat. Passionate
+              about education and changing people's lives through programming.
+              CTO at @Rocketseat. Passionate about education and changing
+              people's lives through programming.
+            </p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={like} alt="like" />
+            </button>
+            <button type="button">
+              <img src={dislike} alt="dislike" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img
+            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
+            alt=""
+          />
+          <footer>
+            <strong>Diego Fernandes</strong>
+            <p>
+              CTO at @Rocketseat. Passionate about education and changing
+              people's lives through programming.
+            </p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={like} alt="like" />
+            </button>
+            <button type="button">
+              <img src={dislike} alt="dislike" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img
+            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
+            alt=""
+          />
+          <footer>
+            <strong>Diego Fernandes</strong>
+            <p>
+              CTO at @Rocketseat. Passionate about education and changing
+              people's lives through programming.
+            </p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={like} alt="like" />
+            </button>
+            <button type="button">
+              <img src={dislike} alt="dislike" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img
+            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
+            alt=""
+          />
+          <footer>
+            <strong>Diego Fernandes</strong>
+            <p>
+              CTO at @Rocketseat. Passionate about education and changing
+              people's lives through programming.
+            </p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={like} alt="like" />
+            </button>
+            <button type="button">
+              <img src={dislike} alt="dislike" />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
