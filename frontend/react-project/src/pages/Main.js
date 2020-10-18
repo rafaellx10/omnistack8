e8b36e9f4a706a30585ad5d90de01a82088f019c
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-10-18 14:24:30 -0300

    add and implent socket.io-client on frontend

--- frontend/react-project/src/pages/Main.js
+++ frontend/react-project/src/pages/Main.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import io from 'socket.io-client';
 
 import api from '../services/api';
 
@@ -25,6 +26,10 @@ export default function Main({ match }) {
     }
 
     loadUsers();
+  }, [match.params.id]);
+
+  useEffect(() => {
+    const socket = io('http://localhost:3333');
   }, [match.params.id]);
 
   async function handleLike(id) {
